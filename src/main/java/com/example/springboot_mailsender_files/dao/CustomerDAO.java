ae0cf4130354dbefbee32bde8d2139d646f055b0
Kvazar103 <nazar.voinarovych250@gmail.com>
2022-12-27 09:17:54 +0200

    init

--- src/main/java/com/example/springboot_mailsender_files/dao/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot_mailsender_files.dao;
-
-import com.example.springboot_mailsender_files.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerDAO extends JpaRepository<Customer,Integer> {
-}
